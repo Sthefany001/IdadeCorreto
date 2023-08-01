5fa4eb3083cf2620b0e60703d026ef08f7bb6396
Sthefany <s532401ed@gmail.com>
2023-08-01 14:25:07 -0400

    first commit

---
+++ Idade (forma simples de fazer)/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade__forma_simples_de_fazer_
+{
+    internal class Aluno
+    {
+    }
+}
---
+++ Idade (forma simples de fazer)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Informe a data de nascimento: ");
+DateTime dataNasc = Convert.ToDateTime(Console.ReadLine());
+DateTime dataAtual = DateTime.Now;
+
+int idade = dataAtual.Year - dataNasc.Year;
+
+if(dataAtual.Month < dataNasc.Month)
+{
+    idade--;
+}
+else if(dataAtual.Month == dataNasc.Month)
+{
+    if(dataAtual.Day < dataNasc.Day)
+    {
+        idade--;   
+    }
+}
+
+Console.WriteLine($"\nIDADE = {idade}");
